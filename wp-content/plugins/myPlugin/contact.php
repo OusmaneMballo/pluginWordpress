97ce5e8ce4b5e02a79611262e546e7aa4b2c502a
OusmaneMballo <ousmanemballo576@gmail.com>
2020-11-25 13:25:57 +0000

    Activation du plugin

---
+++ wp-content/plugins/myPlugin/contact.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * @package myPlugin/contact/
+ * @version 1.7.2
+ */
+/*
+Plugin Name: My ContactForm
+Plugin URI: http://wordpress.org/plugins/myPlugin/contact/
+Description: Vous devez créer un mini site wordpress en y intégrant un formulaire de contact que vous allons créer en tant que plugin
+Author: mballoSoft
+Version: 1.7.2
+Author URI: https://www.github.com/OusmaneMballo/
+*/
+
+
+?>
