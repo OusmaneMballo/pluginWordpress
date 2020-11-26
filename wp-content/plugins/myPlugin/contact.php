28696ec1d7be397b1bebe492413998315b7b46ec
OusmaneMballo <ousmanemballo576@gmail.com>
2020-11-26 15:02:38 +0000

    appel du contact form

--- wp-content/plugins/myPlugin/contact.php
+++ wp-content/plugins/myPlugin/contact.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body> -->
-<form>
+<!-- <form>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputPrenom">Prenom</label>
@@ -46,7 +46,7 @@
     </div>
   </div>
   <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
+</form> -->
 
 
 <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
--- wp-content/plugins/myPlugin/traittement.php
+++ wp-content/plugins/myPlugin/traittement.php
@@ -19,11 +19,11 @@ Author URI: https://www.github.com/OusmaneMballo/
 /* la fonction default_content permet d'ajouter un contenu par defaut 
     lors de la creation d'un  article */
 //add_filter('default_content', 'contenuParDefaut');
-wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'. 'integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"'.'crossorigin="anonymous"' );
-wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
-wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . 'code.jquery.com/code.jquery.com/jquery-3.2.1.slim.min.js'. 'integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"'.' crossorigin="anonymous"', array( 'jquery' ), null, true );
-wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . 'cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'. 'integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"'.' crossorigin="anonymous"', array( 'jquery' ), null, true );
-wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . 'maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'. 'integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"'.' crossorigin="anonymous"', array( 'jquery' ), null, true );
+// wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'. 'integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"'.'crossorigin="anonymous"' );
+// wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+// wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . 'code.jquery.com/code.jquery.com/jquery-3.2.1.slim.min.js'. 'integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"'.' crossorigin="anonymous"', array( 'jquery' ), null, true );
+// wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . 'cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js'. 'integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"'.' crossorigin="anonymous"', array( 'jquery' ), null, true );
+// wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . 'maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'. 'integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"'.' crossorigin="anonymous"', array( 'jquery' ), null, true );
 
 /* la fonction the_content permet d'ajouter un contenu dans la page d'accueil */
 //add_filter('the_content', 'addContenu');
