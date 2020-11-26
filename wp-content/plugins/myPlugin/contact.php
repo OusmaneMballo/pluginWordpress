5e47a0e9052e40a6f2f0a4c392c92358ce3c761c
OusmaneMballo <ousmanemballo576@gmail.com>
2020-11-26 11:09:43 +0000

    Ajout des filters

--- wp-content/plugins/myPlugin/contact.php
+++ wp-content/plugins/myPlugin/contact.php
@@ -12,10 +12,38 @@ Version: 1.7.2
 Author URI: https://www.github.com/OusmaneMballo/
 */
 
-add_action('wp_footer', 'sayHello');
+/* la fonction get_search_form permet d'ajouter une action 
+    apres le champ de recherche */
+//add_action('get_search_form', 'sayHello');
 
-function sayHello(){
+/* la fonction default_content permet d'ajouter un contenu par defaut 
+    lors de la creation d'un  article */
+//add_filter('default_content', 'contenuParDefaut');
+
+/* la fonction the_content permet d'ajouter un contenu dans la page d'accueil */
+add_filter('the_content', 'addContenu');
+add_shortcode('form_contact', 'contactForm');
+function sayHello()
+{
     echo "Hello my plugin wordpress!...";
+}
+
+function contenuParDefaut()
+{
+    return "Mon contenu par defaut
+    Titre 1
+    Titre 2";
+}
+
+function addContenu($content)
+{
+    $content.='<p>Mon contenu du plugin</p>';
+    return $content;
+}
+
+function contactForm()
+{
+    echo "Formulaire de Contact";
 }
 
 ?>
